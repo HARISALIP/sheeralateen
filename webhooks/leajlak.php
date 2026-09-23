93aa66b026896c83fa61353ed7699e9121e8d91b
HARISALIP <harisalip2016@gmail.com>
2026-09-23 19:49:06 +0530

    fix: MySQL Illegal mix of collations on LIKE

--- webhooks/leajlak.php
+++ webhooks/leajlak.php
@@ -62,7 +62,7 @@ try {
         $newLocalStatus = $leajlakStatusMap[strtolower($leajlakStatus)];
 
         $stmt = $db->prepare("SELECT id, order_number, current_status FROM orders WHERE (shopify_order_id = :sid OR order_number = :sid_on OR order_number LIKE CONCAT('%', :sid_like, '%')) AND deleted_at IS NULL LIMIT 1");
-        $stmt->execute([":sid" => $shopifyOrderId, ":sid_on" => $shopifyOrderId, ":sid_like" => $shopifyOrderId]);
+        $stmt->execute([":sid" => $shopifyOrderId, ":sid_on" => $shopifyOrderId, ":sid_like" => "%" . $shopifyOrderId . "%"]);
         $order = $stmt->fetch();
 
         if (!$order) {
@@ -117,6 +117,7 @@ try {
 
 http_response_code(200);
 echo "OK";
+
 
 
 
